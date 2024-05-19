5a6f9da6618639d7f282bad512e2e1450014020e
DinohRatiarisandy <dinohratiarisandy@gmail.com>
2024-05-19 21:57:29 +0300

    reload current scene when player die

--- Scripts/KillZone.cs
+++ Scripts/KillZone.cs
@@ -8,10 +8,14 @@ public partial class KillZone : Area2D
 	public void OnBodyEntered(CharacterBody2D body)
 	{
 		timer = GetNode<Timer>("Timer");
+		Engine.TimeScale = 0.6;
+		body.GetNode<CollisionShape2D>("PlayerCollision").QueueFree();
 		timer.Start();
 	}
 
-	public void OnTimerTimeout() {
+	public void OnTimerTimeout()
+	{
+		Engine.TimeScale = 1;
 		GetTree().ReloadCurrentScene();
 	}
 }
